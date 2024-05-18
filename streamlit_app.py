8de87f6eb469729cf46f36925eb45c871990c3e7
PuriKal <kailas.laxman.puri@gds.ey.com>
2024-05-18 17:22:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME') , col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#covert snowpark dataframe to pandas dataframe
+pd_df = my_dataframe.to_pandas()
+# st.datafram(pd_df)
+#st.stop()
+
 
 incredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -29,6 +35,10 @@ if incredients_list:
     ingredients_string =''
     for fruit_chosen in incredients_list:
         ingredients_string +=fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
